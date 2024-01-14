7be1f108d0ec4110e3aec693b1bfa5cb845b7e68
VASSEUR Laurent <vasseurlaurent@vasseurs-macbook-pro.home>
2024-01-14 10:33:29 +0100

    remove path

--- implementation/aws/local.tf
+++ implementation/aws/local.tf
@@ -2,6 +2,5 @@ locals {
   framework = {
     framework_piece_name     = "3-tier-network"
     framework_implementation = "aws"
-    framework_path           = "${local.framework.framework_piece_name}:${local.framework.framework_implementation}"
   }
 }
